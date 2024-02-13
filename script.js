8dc367af216d99bfb1b182e33515efd7fc3e1763
JuanchoDluffy <66972166+JuanchoDluffy@users.noreply.github.com>
2024-02-12 20:14:35 -0700

    navigation bar

---
+++ script.js
@@ -0,0 +1,6 @@
+function activateMenue() {
+  const menu = document.querySelector(".burger-menu-links");
+  const icon = document.querySelector(".burger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
